2e2cc735bd97e67069f466a5925454821fb3c0ad
abirtasrif <abirtasrif@gmail.com>
2023-08-07 18:57:24 +0600

    added signup

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,7 +5,15 @@ const loginUser = async (req, res) => {
 };
 
 const signupUser = async (req, res) => {
-  res.json({ message: "signup" });
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.signup(email, password);
+
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(400).json({ email: error.message });
+  }
 };
 
 module.exports = {
--- models/userModel.js
+++ models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const Schema = mongoose.Schema;
 
@@ -13,5 +14,20 @@ const userShcema = new Schema({
     required: true,
   },
 });
+
+userShcema.statics.signup = async function (email, password) {
+  const exist = await this.findOne({ email });
+
+  if (exist) {
+    throw Error("Email already used");
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const hashedPass = await bcrypt.hash(password, salt);
+
+  const user = await this.create({ email: email, password: hashedPass });
+
+  return user;
+};
 
 module.exports = mongoose.model("User", userShcema);
